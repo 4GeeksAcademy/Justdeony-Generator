f5efe776a6d8cf6acd1870c6fe25282a6fc1d391
Justdeony <dlopez516@hotmail.com>
2025-11-10 22:27:32 +0000

    Project Done

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+
+
+
+
+function generatorExcuse(){
+  let whoRandom = who[Math.floor(Math.random() * who.length)];
+  let actionRandom = action[Math.floor(Math.random() * action.length)];
+  let whatRandom = what[Math.floor(Math.random() * what.length)];
+  let whenRandom = when[Math.floor(Math.random() * when.length)];
+
+  let excuse = `my excuse: ${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`
+  document.getElementById("excuse").innerHTML = excuse;
+}
+
+
+
+window.onload = generatorExcuse;
+
+
